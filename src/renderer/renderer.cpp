91e48cf49a3c4dd6ce39ef49efa4d758a5297e8b
mrtshv <maxitux@icloud.com>
2023-07-08 10:40:10 +0300

    fix lab 2

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,44 +1,94 @@
-#pragma once
+#include "renderer.h"
 
-#include "settings.h"
-#include "world/camera.h"
-#include "world/model.h"
+#include "utils/error_handler.h"
+
+#ifdef RASTERIZATION
+#include "renderer/rasterizer/rasterizer_renderer.h"
+#endif
+
+#ifdef RAYTRACING
+#include "renderer/raytracer/raytracer_renderer.h"
+#endif
+
+#ifdef DX12
+#include "renderer/dx12/dx12_renderer.h"
+#endif
 
 
-namespace cg::renderer
+using namespace cg::renderer;
+
+void cg::renderer::renderer::set_settings(std::shared_ptr<cg::settings> in_settings)
 {
-	class renderer
-	{
-	public:
-		void set_settings(std::shared_ptr<cg::settings> in_settings);
+	settings = in_settings;
+}
 
-		unsigned get_height();
-		unsigned get_width();
+unsigned cg::renderer::renderer::get_height()
+{
+	return settings->height;
+}
 
-		virtual void init() = 0;
-		virtual void destroy() = 0;
-
-		virtual void update() = 0;
-		virtual void render() = 0;
-
-		void move_forward(float delta = 0.01f);
-		void move_backward(float delta = 0.01f);
-		void move_left(float delta = 0.01f);
-		void move_right(float delta = 0.01f);
-		void move_yaw(float delta = 0.f);
-		void move_pitch(float delta = 0.f);
-
-	protected:
-		std::shared_ptr<cg::settings> settings;
-
-		std::shared_ptr<cg::world::camera> camera;
-		std::shared_ptr<cg::world::model> model;
-
-		std::chrono::time_point<std::chrono::high_resolution_clock> current_time =
-				std::chrono::high_resolution_clock::now();
-		float frame_duration = 0.f;
-	};
+unsigned cg::renderer::renderer::get_width()
+{
+	return settings->width;
+}
 
 
-	extern std::shared_ptr<renderer> make_renderer(std::shared_ptr<cg::settings> settings);
-}// namespace cg::renderer
+std::shared_ptr<renderer> cg::renderer::make_renderer(std::shared_ptr<cg::settings> settings)
+{
+#ifdef RASTERIZATION
+	auto renderer = std::make_shared<cg::renderer::rasterization_renderer>();
+	renderer->set_settings(settings);
+	return renderer;
+#endif
+#ifdef RAYTRACING
+	auto renderer = std::make_shared<cg::renderer::ray_tracing_renderer>();
+	renderer->set_settings(settings);
+	return renderer;
+#endif
+#ifdef DX12
+	auto renderer = std::make_shared<cg::renderer::dx12_renderer>();
+	renderer->set_settings(settings);
+	return renderer;
+#endif
+
+	THROW_ERROR("Type of renderer is not selected");
+}
+
+
+void cg::renderer::renderer::move_forward(float delta)
+{
+	camera->set_position(
+			camera->get_position() +
+			camera->get_direction() * delta * frame_duration);
+}
+
+void cg::renderer::renderer::move_backward(float delta)
+{
+	camera->set_position(
+			camera->get_position() -
+			camera->get_direction() * delta * frame_duration);
+}
+
+void cg::renderer::renderer::move_left(float delta)
+{
+	camera->set_position(
+			camera->get_position()
+			- camera->get_right() * delta * frame_duration);
+}
+
+void cg::renderer::renderer::move_right(float delta)
+{
+	camera->set_position(
+			camera->get_position() +
+			camera->get_right() * delta * frame_duration);
+}
+
+void cg::renderer::renderer::move_yaw(float delta)
+{
+	camera->set_theta(camera->get_theta() + delta);
+}
+
+void cg::renderer::renderer::move_pitch(float delta)
+{
+	camera->set_phi(camera->get_phi() + delta);
+}
