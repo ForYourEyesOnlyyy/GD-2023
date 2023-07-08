bfe2863f032ae92da95b9ceb405da148153f3e09
mrtshv <maxitux@icloud.com>
2023-07-08 10:02:14 +0300

    fix lab 2 errors

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,94 +1,44 @@
-#include "renderer.h"
+#pragma once
 
-#include "utils/error_handler.h"
-
-#ifdef RASTERIZATION
-#include "renderer/rasterizer/rasterizer_renderer.h"
-#endif
-
-#ifdef RAYTRACING
-#include "renderer/raytracer/raytracer_renderer.h"
-#endif
-
-#ifdef DX12
-#include "renderer/dx12/dx12_renderer.h"
-#endif
+#include "settings.h"
+#include "world/camera.h"
+#include "world/model.h"
 
 
-using namespace cg::renderer;
-
-void cg::renderer::renderer::set_settings(std::shared_ptr<cg::settings> in_settings)
+namespace cg::renderer
 {
-	settings = in_settings;
-}
+	class renderer
+	{
+	public:
+		void set_settings(std::shared_ptr<cg::settings> in_settings);
 
-unsigned cg::renderer::renderer::get_height()
-{
-	return settings->height;
-}
+		unsigned get_height();
+		unsigned get_width();
 
-unsigned cg::renderer::renderer::get_width()
-{
-	return settings->width;
-}
+		virtual void init() = 0;
+		virtual void destroy() = 0;
 
+		virtual void update() = 0;
+		virtual void render() = 0;
 
-std::shared_ptr<renderer> cg::renderer::make_renderer(std::shared_ptr<cg::settings> settings)
-{
-#ifdef RASTERIZATION
-	auto renderer = std::make_shared<cg::renderer::rasterization_renderer>();
-	renderer->set_settings(settings);
-	return renderer;
-#endif
-#ifdef RAYTRACING
-	auto renderer = std::make_shared<cg::renderer::ray_tracing_renderer>();
-	renderer->set_settings(settings);
-	return renderer;
-#endif
-#ifdef DX12
-	auto renderer = std::make_shared<cg::renderer::dx12_renderer>();
-	renderer->set_settings(settings);
-	return renderer;
-#endif
+		void move_forward(float delta = 0.01f);
+		void move_backward(float delta = 0.01f);
+		void move_left(float delta = 0.01f);
+		void move_right(float delta = 0.01f);
+		void move_yaw(float delta = 0.f);
+		void move_pitch(float delta = 0.f);
 
-	THROW_ERROR("Type of renderer is not selected");
-}
+	protected:
+		std::shared_ptr<cg::settings> settings;
+
+		std::shared_ptr<cg::world::camera> camera;
+		std::shared_ptr<cg::world::model> model;
+
+		std::chrono::time_point<std::chrono::high_resolution_clock> current_time =
+				std::chrono::high_resolution_clock::now();
+		float frame_duration = 0.f;
+	};
 
 
-void cg::renderer::renderer::move_forward(float delta)
-{
-	camera->set_position(
-			camera->get_position() +
-			camera->get_direction() * delta * frame_duration);
-}
-
-void cg::renderer::renderer::move_backward(float delta)
-{
-	camera->set_position(
-			camera->get_position() -
-			camera->get_direction() * delta * frame_duration);
-}
-
-void cg::renderer::renderer::move_left(float delta)
-{
-	camera->set_position(
-			camera->get_position()
-			- camera->get_right() * delta * frame_duration);
-}
-
-void cg::renderer::renderer::move_right(float delta)
-{
-	camera->set_position(
-			camera->get_position() +
-			camera->get_right() * delta * frame_duration);
-}
-
-void cg::renderer::renderer::move_yaw(float delta)
-{
-	camera->set_theta(camera->get_theta() + delta);
-}
-
-void cg::renderer::renderer::move_pitch(float delta)
-{
-	camera->set_phi(camera->get_phi() + delta);
-}
+	extern std::shared_ptr<renderer> make_renderer(std::shared_ptr<cg::settings> settings);
+}// namespace cg::renderer
